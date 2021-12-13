b8925e2c4b225ef0746f2cda363c1e39d4e8b394
Josue Tavara <joshuatavara23@gmail.com>
2021-12-13 17:40:48 -0500

    1.6) type class variance in cats

---
+++ src/main/scala/variance/ControllingInstanceSelection.scala
@@ -0,0 +1,57 @@
+package variance
+
+import sandbox.{JsInt, JsObject, JsString}
+
+
+object ControllingInstanceSelection extends App {
+
+
+
+  /*
+  * -- COVARIANCE:
+  *    trait F[+A] // the "+" means "covariant"
+  *
+  * allows us to substitute collections of one type with a collection
+  * of a subtype data that we can later get out of a container type
+  * such as List, or otherwise returned by some method. (READ OPERATIONS)
+  * */
+
+  sealed trait Shape
+  case class Circle(radius: Double) extends Shape
+  val circles: List[Circle] = List(Circle(2.3))
+  val shapes: List[Shape] = circles
+
+
+
+  /*
+  * -- Contravariance:
+  *    trait F[-A] // the "-" means "Contravariance"
+  *
+  * allows us modelling types that represent inputs
+  * */
+
+  import sandbox.JsonSerializer
+  import sandbox.Json
+
+  val shape: Shape = Circle(3.3)
+  val circle: Circle = Circle(2.4)
+  val shapeWriter: JsonSerializer[Shape] = (value: Shape) => JsString(value.toString)
+  val circleWriter: JsonSerializer[Circle] = (value: Circle) => JsObject(
+    Map("radio" -> JsInt(value.radius.toInt))
+  )
+  def format[A](value: A, serializer: JsonSerializer[A]): Json = {
+    serializer.serialize(value)
+  }
+  //This means we can use "shapeWriter"
+  //anywhere we expect to see a "JsonWriter[Circle]"
+  println(format(circle, shapeWriter))
+  println(format(circle, circleWriter))
+
+
+  /*
+  * Type Class Variance        | Invariant | Covariant | Contravariant
+    Supertype instance used?    |  No      |No         |     Yes
+    More specific type preferred? | No     | Yes        | No
+  * */
+}
+
