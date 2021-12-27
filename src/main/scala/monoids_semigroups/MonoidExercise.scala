2511b96df6f0f345351b833403b8a075331760d8
Josue Tavara <joshuatavara23@gmail.com>
2021-12-27 16:58:43 -0500

    monoid syntax and monoid exercise

---
+++ src/main/scala/monoids_semigroups/MonoidExercise.scala
@@ -0,0 +1,49 @@
+package monoids_semigroups
+
+
+
+object MonoidExercise extends App {
+
+  import cats.Monoid
+  import cats.syntax.semigroup._ // for |+|
+
+  //--------------------------------------------------
+  def add[A](items: List[A])(implicit monoid: Monoid[A] ): A =
+    items.foldLeft(monoid.empty)(_ |+| _)
+  //--------------------------------------------------
+  //  We can optionally use Scala’s context bound syntax to write the same code in
+  //   a shorter way:
+  def addV2[A: Monoid](items: List[A]): A =
+    items.foldLeft(Monoid[A].empty)(_ |+| _)
+  //--------------------------------------------------
+
+  import cats.instances.int._ // for Monoid
+  add(List(1, 2, 3))
+  // res10: Int = 6
+
+  //--------------------------------------------------
+  import cats.instances.option._ // for Monoid
+  add(List(Some(1), Some(2), Some(3))) // this is wrong
+  /*
+    error: could not find implicit value for evidence parameter of type
+      cats.Monoid[Some[Int]]
+     add(List(Some(1), Some(2), Some(3)))
+     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+     This happens because the inferred type of the list is List[Some[Int]],
+     while Cats will only generate a Monoid for Option[Int].
+   */
+  //--------------------------------------------------
+
+  case class Order(totalCost: Double, quantity: Double)
+  implicit val monoid: Monoid[Order] = new Monoid[Order] {
+    def combine(o1: Order, o2: Order) =
+      Order(
+        o1.totalCost + o2.totalCost,
+        o1.quantity + o2.quantity
+      )
+    def empty = Order(0, 0)
+  }
+
+  add(List(Order(10,2), Order(3,3)))
+  // res11:Order = Order(13.0,5.0)
+}
---
+++ src/main/scala/monoids_semigroups/MonoidSyntax.scala
@@ -0,0 +1,19 @@
+package monoids_semigroups
+
+import cats.Monoid
+
+object MonoidSyntax extends App{
+
+
+  import cats.instances.string._ // for Monoid
+  import cats.syntax.semigroup._ // as COMBINE comes from semigroup we can use |+|
+  val stringResult = "Hi " |+| "there" |+| Monoid[String].empty
+  // stringResult: String = "Hi there"
+
+  //
+  import cats.instances.int._ // for Monoid
+  val intResult = 1 |+| 2 |+| Monoid[Int].empty
+  // intResult: Int = 3
+}
+
+
